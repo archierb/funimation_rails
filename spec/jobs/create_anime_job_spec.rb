038a705a43b5d080ec82e954965d0b941c88a149
Francisco Paradella <franciscoleite.dev@protonmail.com>
2021-10-02 14:08:16 -0300

    test: Add testing to Anime create rake

--- spec/jobs/create_anime_job_spec.rb
+++ spec/jobs/create_anime_job_spec.rb
@@ -1,5 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CreateAnimeJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'return correct active record object' do
+    before do
+      allow(::AnimesServices::CreateAnimeService).to receive(:new) {
+                                                       lambda {
+                                                         Anime.create(name: 'Claymore', debut_date: '2007-04-04', synopsis: 'Synopsis', poster_image: 'Poster Image',
+                                                                      cover_image: 'Cover Image')
+                                                       }
+                                                     }
+    end
+
+    it 'returns a correct anime name, debut_date, synopsis, poste_image and cover_image' do
+      perform_job = CreateAnimeJob.perform_now(anime_name: 'Claymore')
+      expect(perform_job.name).to eq('Claymore')
+      expect(perform_job.debut_date).to eq('2007-04-04')
+      expect(perform_job.synopsis).to eq('Synopsis')
+      expect(perform_job.poster_image).to eq('Poster Image')
+      expect(perform_job.cover_image).to eq('Cover Image')
+    end
+  end
 end
